aee77cf08e961c562bf1e12c4b25f03111afd224
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-20 17:46:11 +0530

    Added new transporter to logger to save log data to Mongo DB.

--- src/logger/dev/devLogger.ts
+++ src/logger/dev/devLogger.ts
@@ -3,6 +3,10 @@ import path from "path";
 import winston, { Logger, format } from "winston";
 const { combine, timestamp } = format;
 
+// Module to save logs to MongoDB
+import { MongoDB } from "winston-mongodb";
+require("winston-mongodb");
+
 // Type for the logger configuration
 export interface LoggerConfig {
   level: string;
@@ -33,6 +37,16 @@ const devLogger = (): Logger => {
       // Logger instance to log all logs to log file in logs directory.
       new winston.transports.File({
         filename: path.join(devLogDirectory, "complete.log"),
+      }),
+
+      // Logger instance to log all logs to MongoDB.
+      new winston.transports.MongoDB({
+        level: "debug",
+        db: process.env.MONGO_DB_URI!,
+        options: { useUnifiedTopology: true },
+        collection: "server_logs",
+        storeHost: true,
+        format: combine(timestamp(), winston.format.json()),
       }),
     ],
   };

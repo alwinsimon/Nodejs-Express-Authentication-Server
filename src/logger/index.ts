000ad7f814a3307ebeaefea1bb5edb3b771037b0
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-20 16:59:44 +0530

    Configured production logger.

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,12 +1,18 @@
 // ================== Logger Configurations =================
 
 import { Logger } from "winston";
+
 import devLogger from "./dev/devLogger";
+import prodLogger from "./prod/prodLogger";
 
 let logger: Logger | null = null;
 
 if (process.env.NODE_ENV === "development") {
   logger = devLogger();
+}
+
+if (process.env.NODE_ENV === "production") {
+  logger = prodLogger();
 }
 
 export default logger;

fcc36a19fe1d96450da34a187fc5925611cb1c09
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-21 00:35:57 +0530

    Configured streaming of HTTP logs to winston logger.

--- src/logger/HTTP-request-logger.ts
+++ src/logger/HTTP-request-logger.ts
@@ -1,11 +1,24 @@
 import morgan from "morgan";
+import logger from ".";
 
 const morganLogger = () => {
-  const logger = morgan(
-    '[:date[web]] :remote-addr - :remote-user :method :url HTTP/:http-version :status :total-time[digits]ms :res[content-length] ":referrer" ":user-agent"'
+  // Create a custom stream that writes to both Winston and console
+  const dualStream = {
+    write: (message: string) => {
+      // Log to Winston
+      logger!.info(message.trim());
+      // Log to console
+      console.log(message.trim());
+    },
+  };
+
+  // Use the custom stream with morgan middleware
+  const httpLogger = morgan(
+    '[:date[web]] :remote-addr - :remote-user :method :url HTTP/:http-version :status :total-time[digits]ms :res[content-length] ":referrer" ":user-agent"',
+    { stream: dualStream }
   );
 
-  return logger;
+  return httpLogger;
 };
 
 export default morganLogger;

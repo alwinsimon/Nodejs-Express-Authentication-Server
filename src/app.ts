4e411ce8b27932c851beedf55043122ce5bc31af
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-23 17:44:36 +0530

    Modified Health Check Route to send date and time in response.

--- src/app.ts
+++ src/app.ts
@@ -29,14 +29,29 @@ app.use(cookieParser());
 // Function to provide swagger documentation
 generateSwaggerDocs(app, PORT as number);
 
-//? ===================== Application Home Route =====================
+//? ===================== Application Health Check Route =====================
 app.get("/health", (req, res) => {
+  const currentDate = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
+  };
+  const formattedDate = currentDate.toLocaleString("en-US", options);
+
   res.status(200).json({
-    status: `${process.env.APPLICATION_NAME} and Systems are Up & Running.`,
+    Status: `${process.env.APPLICATION_NAME} and Systems are Up & Running.`,
+    Date_Time: formattedDate,
   });
 });
 
 //? ===================== Routes Configuration =====================
+// =====================V1 APIs Routes Configuration =================
 app.use("/api/v1", v1APIs);
 
 // Resource Not Found Error Configuration

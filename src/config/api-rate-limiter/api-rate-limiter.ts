dfb93992ecd2792968adbc18ce0ad8e9a8ac40b2
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-25 00:08:38 +0530

    Modified rate limiter duration.

--- src/config/api-rate-limiter/api-rate-limiter.ts
+++ src/config/api-rate-limiter/api-rate-limiter.ts
@@ -1,8 +1,8 @@
 import { rateLimit } from 'express-rate-limit'
 
 const apiRateLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	limit: 60, // Limit each IP to 60 requests per `window` (here, per 1 minute), ie, 1 request per 1 second.
+	windowMs: 10 * 60 * 1000, // 10 minute
+	limit: 600, // Limit each IP to 600 requests per `window` (here, per 10 minute).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Use an external store for consistency across multiple server instances.
